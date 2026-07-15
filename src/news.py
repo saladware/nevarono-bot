689cace483d6acb1448d31bf5bd7bef279404b5d
Evgeny Goryachev <saladware46@gmail.com>
2026-07-15 22:49:46 +0300

    feat(news): implement string representation and hashtag generation for NewsItem

--- src/news.py
+++ src/news.py
@@ -17,3 +17,21 @@ class NewsItem(NamedTuple):
     category: str
     author: Author
     summary: str
+    keywords: "list[str]" = []  # noqa: RUF012 temp
+
+    def __str__(self) -> str:
+        important_mark = "⚠️ " if self.is_important else ""
+        keyword_hashtags = " ".join(map(_hashtag, self.keywords))
+        published_str = self.published_at.strftime("%d.%m.%Y  %H:%M")
+        return (
+            f"{important_mark}"
+            f'<b><a href="{self.url}">{self.title}</a></b>\n\n'
+            f"{self.summary}\n\n"
+            f"{published_str}\n\n"
+            f"{_hashtag(self.category)} {keyword_hashtags}"
+        )
+
+
+def _hashtag(keyword: str) -> str:
+    snaked_keyword = "_".join(keyword.split())
+    return f"#{snaked_keyword}"
